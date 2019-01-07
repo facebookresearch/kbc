989686b9194fc7f5f76a836d827ee9858310dce3
Timothee Lacroix <timothee.lax@gmail.com>
2019-01-07 01:55:17 -0800

    adding fb copyright to download_data.sh

--- kbc/scripts/download_data.sh
+++ kbc/scripts/download_data.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+#
 
 # Download FB15K, FB15K-237, WN, WN18RR and YAGO3-10
 
